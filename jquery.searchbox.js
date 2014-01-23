e394047b6af11b22fcebaa8afd4deb6d2dce4a5b
Nattawat Nonsung <armmer1@gmail.com>
2014-01-24 00:37:41 +0700

    First comment with inital feature.

---
+++ jquery.searchbox.js
@@ -0,0 +1,52 @@
+/**
+* Use immediately Invoked Function Expression to
+* - Prevent conflicting with other libary on alias $
+* - Scode varaible to be private
+*/
+(function( $ ) {
+  $.fn.searchbox = function(options) {
+
+		// Define default setting
+		var settings = $.extend({
+			width: 150,
+			duration: 300
+    }, options );
+
+		var width = this.width();
+
+  //   this.css({
+  //   	"-webkit-transition": ("width "+settings.duration+"s"),
+  //   	"-moz-transition": ("width "+settings.duration+"s"),
+  //   	"transition": ("width "+settings.duration+"s"),
+  //   	"width": width
+		// });
+
+		this.on("focus", function(){
+			// $(this).css({width: settings.width});
+			$(this).animate({
+	      width: settings.width
+	    }, settings.duration, function(){
+	      // callback function
+	    });
+		});
+
+		this.on("blur", function(){
+			$(this).animate({
+	      width: width
+	    }, settings.duration, function(){
+	      // callback function
+	    });
+		});
+
+		if(settings.action && typeof(settings.action) == "function"){
+			this.on('keypress', function(e) {
+				if (e.which === 13) { // press enter
+					settings.action($(this).val());
+				}
+			});
+		}
+		// Return jQuery so that it's chainable 
+		return this;		
+  };
+ 
+}( jQuery ));
